1ea05c4e8714b07b243a58b8c9bb964eb08b76cb
jambackie <jambackie@gmail.com>
2022-09-02 23:40:35 +0300

    fix request

--- src/models/baseCode.js
+++ src/models/baseCode.js
@@ -1,7 +1,12 @@
 export default async function () {
-  let code = await fetch('http://ip-api.com/json')
-  code = await code.json()
-  code = code.countryCode.toUpperCase()
+  let code = ''
+  try {
+    code = await fetch('http://ip-api.com/json')
+    code = await code.json()
+    code = code.countryCode.toUpperCase()
+  } catch (e) {
+    console.error(e)
+  }
 
   const lang = navigator.language.slice(0, 2).toUpperCase()
 
